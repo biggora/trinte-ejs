f3f08a3c93fe84299c7935a7fb572e812752911f
Alex <Alex@Alex-PC>
2013-09-26 10:09:14 +0300

    Write README

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-/** 
- * @project  trinte-ejs
- * @author   trinte-ejs
- * @created  2013-09-24 03:43:26
+/**
+ *  TrinteJS Ejs Templates
+ *
+ *  @project     TrinteJS
+ *  @version     0.0.1
+ *  @package     trinte-ejs
+ *  @author      Aleksejs Gordejevs
+ *  @created     2013-09-24 03:12:02
  * 
- * 
- */
+ *  Created by init script
+ *  App based on TrinteJS MVC framework
+ *  TrinteJS homepage http://www.trintejs.com
+ **/
 
 module.exports = require('./lib/trinte-ejs.js');
